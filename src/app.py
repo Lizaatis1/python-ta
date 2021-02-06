58be36ce801e310f3d4d4386d7f4a56e5c707cdc
Lizaatis1 <lizaatis.github.com>
2021-02-06 21:39:03 +0300

    Add solution

--- src/app.py
+++ src/app.py
@@ -3,14 +3,18 @@ import os
 
 
 def palindrome(s):
-    # your code goes here
+    if s == s[::-1]:
+        return s + " is a palindrome"
+    return s + " is not a palindrome"
 
+ 
 def solution(s):
     return palindrome(s)
 
 
 if __name__ == "__main__":
-    if len(sys.argv) <= 1:
+ if len(sys.argv) <= 1:
         sys.exit(os.error("Argment required"))
-
-    print(solution(sys.argv[1]))
+        
+        print(solution(sys.argv[1]))
+    
